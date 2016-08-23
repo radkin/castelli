7df4424233fa61baf00b7783ac98598db2151fb0
Noel Miller <noeldamonmiller@gmail.com>
2016-08-23 10:16:48 -0700

    Update submit_castelli_artifact.sh
    just tweaked for example

--- jenkins_config/submit_castelli_artifact.sh
+++ jenkins_config/submit_castelli_artifact.sh
@@ -9,4 +9,4 @@ upload_job_name=`echo "${upload_build_url}" | cut -d'/' -f6`
 echo "our upload_job_name is ${upload_job_name}"
 
 # submit to castelli via REST service
-curl -X POST -d "release=${release}&artifacts_urls=${artifacts_urls}&job_name=${upload_job_name}&job_url=${upload_build_url}" -H "Content-Type: application/x-www-form-urlencoded" http://tools-dev.devops.fds.com:3000/api/jenkins
+curl -X POST -d "release=${release}&artifacts_urls=${artifacts_urls}&job_name=${upload_job_name}&job_url=${upload_build_url}" -H "Content-Type: application/x-www-form-urlencoded" http://castelli-host.example.com:3000/api/jenkins
